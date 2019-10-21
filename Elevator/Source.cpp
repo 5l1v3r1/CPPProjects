0305dbd71888d2a576cdfa4d967a120b1e0f6702
DerekCresswell <derekjcress@gmail.com>
2019-10-20 23:24:19 -0700

    Added basic setup for Elevator project

---
+++ Elevator/Source.cpp
@@ -0,0 +1,19 @@
+//Manager
+// ^ Stores floors and passengers, designates which elevator holds which tokens,
+// if the others can switch direction
+//Make Elevator class that can expand past 2 and 10 floors
+
+#include <iostream>
+
+int main() {
+
+	std::cout << "Hello";
+
+}
+
+/*
+10 floors
+Graphic print out
+Don't switch direction until all request are satisfied
+Only pick up passengers going in the same direction
+*/
