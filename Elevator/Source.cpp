83c90b55720f17fa8354c8c3cd3c544c86ae7b85
DerekCresswell <derekjcress@gmail.com>
2019-10-20 23:24:59 -0700

    Expanded setup to include basic plan

--- Elevator/Source.cpp
+++ Elevator/Source.cpp
@@ -4,12 +4,59 @@
 //Make Elevator class that can expand past 2 and 10 floors
 
 #include <iostream>
+#include <vector>
 
 int main() {
 
 	std::cout << "Hello";
 
 }
+
+class ElevatorManager {
+
+	public:
+		unsigned int floors;
+		//Make array :(;
+		std::vector<bool> tokens;
+
+		ElevatorManager(unsigned int floors) {
+			this->floors = floors;
+		}
+
+		void addElevator(Elevator toAdd) {
+			elevators.push_back(toAdd);
+		}
+
+		void addElevator(unsigned int capacity) {
+			elevators.push_back();
+		}
+
+		bool passToken(unsigned int elevatorNum, unsigned int floorNum) {
+
+			floorNum = floorNum < floors ? floorNum : floors;
+			if(tokens[floorNum]) {
+				tokens[floorNum] = false;
+				return true;
+			}
+				return false;
+
+		}
+
+	private:
+		std::vector<Elevator> elevators;
+
+};
+
+class Elevator {
+
+	public:
+		unsigned int capacity;
+
+		Elevator(unsigned int capacity) {
+			this->capacity = capacity;
+		}
+
+};
 
 /*
 10 floors
